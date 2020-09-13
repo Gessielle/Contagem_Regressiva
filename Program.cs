d890ea7055ee20a9a74d9ba4c193638fc75ea8ab
gessielle-barossi@hotmail.com <Gessielle-barossi@hotmail.com>
2020-09-13 19:13:43 -0300

    Contagem Regressiva

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Contagem_Regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Write("\n");
+
+            Console.WriteLine("Apontar...");
+            Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadKey();
+            Console.Write("\n");
+
+            Console.WriteLine("FOGO!");
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+        }
+    }
+}
